c15e79d1ddf3eedeb45002f8e492b711d1fafe7e
Данила Тяпкин <dtapkin6@gmail.com>
2024-05-06 18:32:53 +0300

    Добавьте файлы проекта.

---
+++ Laba №11/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int nowDay = 0;
+
+        Day day = () =>
+        {
+            string[] daysWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            string nextDay = daysWeek[nowDay];
+
+            nowDay = (nowDay + 1) % 7;
+
+            return nextDay;
+        };
+
+        for (byte i = 0; i < 15; i++)
+            Console.WriteLine(day());
+    }
+}
+
+delegate string Day();
